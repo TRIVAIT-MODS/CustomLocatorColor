fd51de972f46ad2d328703d092b89a6ce41bac98
TRIVAIT <dimitmapper@gmail.com>
2026-07-05 14:27:30 +0400

    version 1.0.0 mc 26.1

--- src/main/java/org/trivait/customlocatorcolor/CustomLocatorColor.java
+++ src/main/java/org/trivait/customlocatorcolor/CustomLocatorColor.java
@@ -1,12 +1,21 @@
 package org.trivait.customlocatorcolor;
 
 import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.ConfigManager;
+import me.shedaniel.autoconfig.gui.ConfigScreenProvider;
+import me.shedaniel.autoconfig.gui.registry.ComposedGuiRegistryAccess;
+import me.shedaniel.autoconfig.gui.registry.DefaultGuiRegistryAccess;
+import me.shedaniel.autoconfig.gui.registry.api.GuiRegistryAccess;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.trivait.customlocatorcolor.config.Config;
+
+import java.util.function.Supplier;
 
 public class CustomLocatorColor implements ClientModInitializer {
 	public static final String MOD_ID = "customlocatorcolor";
@@ -20,7 +29,7 @@ public class CustomLocatorColor implements ClientModInitializer {
 		CONFIG = AutoConfig.getConfigHolder(Config.class).getConfig();
 	}
 
-	public static ResourceLocation id(String path) {
-		return ResourceLocation.fromNamespaceAndPath(MOD_ID, path);
+	public static Identifier id(String path) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, path);
 	}
 }
--- src/main/java/org/trivait/customlocatorcolor/config/ModMenuIntegration.java
+++ src/main/java/org/trivait/customlocatorcolor/config/ModMenuIntegration.java
@@ -2,13 +2,24 @@ package org.trivait.customlocatorcolor.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
-import net.minecraft.client.gui.screens.Screen;
+import me.shedaniel.autoconfig.AutoConfigClient;
+import me.shedaniel.clothconfig2.api.AbstractConfigListEntry;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import me.shedaniel.clothconfig2.gui.entries.NestedListListEntry;
+import net.minecraft.network.chat.Component;
+import org.trivait.customlocatorcolor.CustomLocatorColor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ModMenuIntegration implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> (Screen) AutoConfig.getConfigScreen(Config.class, parent).get();
+        return parent -> {
+            return AutoConfigClient.getConfigScreen(Config.class, parent).get();
+        };
     }
 }
